dfedcbd4dfc3a760afed0bee93b9ca91e264f429
adepusa <60025672+adepusa@users.noreply.github.com>
2020-02-14 11:59:00 +0530

    nexus lab

---
+++ NexusLab2/src/main/java/com/broadridge/App.java
@@ -0,0 +1,13 @@
+package com.broadridge;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
